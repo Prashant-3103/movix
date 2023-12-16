aae259fe63fb4bf22721e98de0b238aba6ac0a80
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-12-16 23:37:55 +0530

    header

--- src/pages/home/HeroBanner/HeroBanner.jsx
+++ src/pages/home/HeroBanner/HeroBanner.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import './style.css'
+import './style.scss'
+import useFetch from '../../../hooks/useFetch'
+import { useSelector } from 'react-redux'
+import Img from '../../../components/lazyLoadImage/Img'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 const HeroBanner = () => {
     const navigate = useNavigate()
     const [background, setBackground]=useState("")
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState()
+    const {url}= useSelector((state)=>state.home)
+    const {data, loading} = useFetch("/movie/upcoming")
+    useEffect(()=>{
+const bg =url?.backdrop+ data?.results?.[Math.floor(Math.random()*20)]?.backdrop_path
+setBackground(bg)
+    },[data])
     const searchQueryHandler = (event)=>{
 if(event.key==="Enter"&& query.length>0){
 navigate(`/search/${query}`)
@@ -12,7 +22,15 @@ navigate(`/search/${query}`)
     }
   return (
    <div className='heroBanner'>
-<div className='wrapper'>
+  { !loading &&( <div className='backdrop-img'>
+<Img src={background}/>
+    </div>)}
+
+<div className="opacity-layer">
+
+</div>
+
+<ContentWrapper>
 <div className='heroBannerContent'>
 <span className='title'>Welcome</span>
 <span className='subtitle'>Millions of movies , TV shows and people to discover.
@@ -22,7 +40,9 @@ Explore now</span>
     <button>Search</button>
 </div>
 </div>
-</div>
+</ContentWrapper>
+
+
    </div>
   )
 }
